fd36556437c87d6bf7f585a0761fca1a8fa5f33f
Filippo Graziano <filippograziano13@gmail.com>
2026-07-02 14:23:55 +0200

    add style to information section

--- src/components/jsx-comnponents/footer.jsx
+++ src/components/jsx-comnponents/footer.jsx
@@ -56,53 +56,72 @@ const Footer = () => (
         </section>
 
         <section id='information'>
+
             <div className="container">
-                <ul>
-                    <li>
-                        <h2>DC Comics</h2>
-                    </li>
-                    <li><a href="#">Charset</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
-                </ul>
-                <ul>
-                    <li>
-                        <h2>Shop</h2>
-                    </li>
-                    <li><a href="#">Shop DC</a></li>
-                    <li><a href="#">Shop DC Collectibles</a></li>
-                </ul>
-                <ul>
-                    <li>
-                        <h2>DC</h2>
-                    </li>
-                    <li><a href="#">Terms Of Use</a></li>
-                    <li><a href="#">Privacy policy {`(New)`}</a></li>
-                    <li><a href="#">Ad Choices</a></li>
-                    <li><a href="#">Advertising</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Subscriptions</a></li>
-                    <li><a href="#">Talent Workshops</a></li>
-                    <li><a href="#">CPSC Certificates</a></li>
-                    <li><a href="#">Ratings</a></li>
-                    <li><a href="#">Shop Help</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-                <ul>
-                    <li>
-                        <h2>Sites</h2>
-                    </li>
-                    <li><a href="#">DC</a></li>
-                    <li><a href="#">MAD Magazine</a></li>
-                    <li><a href="#">DC Kids</a></li>
-                    <li><a href="#">DC Universe</a></li>
-                    <li><a href="#">DC Power Visa</a></li>
-                </ul>
+
+                <div id="link">
+
+                    <ul>
+
+                        <li>
+                            <h2>DC Comics</h2>
+                        </li>
+                        <li><a href="#">Charset</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+
+                    </ul>
+
+                    <ul>
+
+                        <li>
+                            <h2>Shop</h2>
+                        </li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+
+                    </ul>
+
+                    <ul>
+
+                        <li>
+                            <h2>DC</h2>
+                        </li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy {`(New)`}</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+
+                    </ul>
+
+                    <ul>
+
+                        <li>
+                            <h2>Sites</h2>
+                        </li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+
+                    </ul>
+
+                </div>
+
             </div>
+
         </section>
 
         <section id='social'>
