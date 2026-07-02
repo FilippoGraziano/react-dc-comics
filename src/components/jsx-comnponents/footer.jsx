499258aaeff3883a6fd13f968e3678cd9bbd6efd
Filippo Graziano <filippograziano13@gmail.com>
2026-07-02 14:41:34 +0200

    add style to social section

--- src/components/jsx-comnponents/footer.jsx
+++ src/components/jsx-comnponents/footer.jsx
@@ -66,13 +66,34 @@ const Footer = () => (
                         <li>
                             <h2>DC Comics</h2>
                         </li>
-                        <li><a href="#">Charset</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
+
+                        <li>
+                            <a href="#">Charset</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+
+                        <li>
+                            <a href="#">News</a>
+                        </li>
 
                     </ul>
 
@@ -81,8 +102,14 @@ const Footer = () => (
                         <li>
                             <h2>Shop</h2>
                         </li>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
+
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
 
                     </ul>
 
@@ -91,17 +118,51 @@ const Footer = () => (
                         <li>
                             <h2>DC</h2>
                         </li>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy policy {`(New)`}</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contact Us</a></li>
+
+                        <li>
+                            <a href="#">Terms Of Use</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Privacy policy {`(New)`}</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Talent Workshops</a>
+                        </li>
+
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Contact Us</a>
+
+                        </li>
 
                     </ul>
 
@@ -110,11 +171,26 @@ const Footer = () => (
                         <li>
                             <h2>Sites</h2>
                         </li>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
+
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
 
                     </ul>
 
@@ -127,39 +203,53 @@ const Footer = () => (
         <section id='social'>
 
             <div className="container">
+
                 <button>Sing-up Now!</button>
-                <div>Follow US</div>
+
                 <nav>
+
                     <ul>
+
+                        <li>
+                            <p>Follow US</p>
+                        </li>
+
                         <li>
                             <a href="#">
                                 <img src="img/footer-facebook.png" alt="facebook icon" />
                             </a>
                         </li>
+
                         <li>
                             <a href="#">
                                 <img src="img/footer-twitter.png" alt="teitter icon" />
                             </a>
                         </li>
+
                         <li>
                             <a href="#">
                                 <img src="img/footer-youtube.png" alt="youtube icon" />
                             </a>
                         </li>
+
                         <li>
                             <a href="#">
                                 <img src="img/footer-pinterest.png" alt="pinterest icon" />
                             </a>
                         </li>
+
                         <li>
                             <a href="#">
                                 <img src="img/footer-periscope.png" alt="periscope icon" />
                             </a>
                         </li>
+
                     </ul>
+
                 </nav>
 
             </div>
+
         </section>
 
     </footer>
