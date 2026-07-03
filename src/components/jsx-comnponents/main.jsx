208df5b1ce21ad66c55a21144a9c834e1a61c1d4
Filippo Graziano <filippograziano13@gmail.com>
2026-07-04 01:01:55 +0200

    implement logic tha create card of the comics substitute the wrong url of img add button, span and jumbo add style to the main

--- src/components/jsx-comnponents/main.jsx
+++ src/components/jsx-comnponents/main.jsx
@@ -1,14 +1,41 @@
 
 import '../css-comnponents/main.css'
-import '../comics'
+import comics from '../comics';
+
+const comicsCopy = [...comics];
+
+const wrongComicCover = comicsCopy.find(element => (element.series === `Batman`))
+wrongComicCover.thumb = comicsCopy.find(element => (element.series === `Batman Beyond`)).thumb
 
 const Main = () => (
     <main>
+
+        <div id="jumbo"></div>
+
         <div className="container">
-            
-            {`--> Content goes here <--`}
+
+            <span>Current series</span>
+
+            <section>
+
+                {comicsCopy.map(element => (
+
+                    <div key={element.id} className="card">
+
+                        <div className='comics-cover' style={{backgroundImage: `url(${element.thumb})`}}></div>
+
+                        <p>{element.series}</p>
+
+                    </div>
+
+                ))}
+
+            </section>
+
+            <button>Load more</button>
 
         </div>
+        
     </main>
 );
 
